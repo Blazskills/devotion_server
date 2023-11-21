a25cb9709e5633fa168e82906ef323fd7ad0d886
Blazskills <ilesanmiisaac@gmail.com>
2023-11-21 14:10:36 +0100

    fixing os errors

--- hymnproject/settings/production.py
+++ hymnproject/settings/production.py
@@ -7,14 +7,16 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 USE_X_FORWARDED_HOST = True
 
 
-SECRET_KEY = env(
+SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY",
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG")
+# DEBUG = env.bool("DJANGO_DEBUG")
+DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "true"
 
-DATABASES = {"default": env.db("DATABASE_URL")}
+# DATABASES = {"default": os.environ.get.db("DATABASE_URL")}
+DATABASES["default"] = dj_database_url.parse(os.environ.get.db("DATABASE_URL"))
 
 # CSRF_TRUSTED_ORIGINS = ["http://localhost:8080","http://localhost:5173",]
 DOMAIN = "Devotion"
