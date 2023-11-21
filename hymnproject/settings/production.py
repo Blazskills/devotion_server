1dfb3b9bc61402bb0e31a42585ee123a447fe489
Blazskills <ilesanmiisaac@gmail.com>
2023-11-21 15:13:18 +0100

    feat: added whitenoise try 2

--- hymnproject/settings/production.py
+++ hymnproject/settings/production.py
@@ -23,4 +23,4 @@ DATABASES = {"default": env.db("DATABASE_URL")}
 DOMAIN = "Devotion"
 SITE_NAME = "Devotion"
 ALLOWED_HOSTS = ["*"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
